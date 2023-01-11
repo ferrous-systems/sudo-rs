144100a8d317798048a0324a0b069f1b793b5b78
Brigitte Markmann <brigitte.markmann@ferrous-systems.com>
2023-01-11 16:31:19 +0100

    Parse CLI for arguments using `clap`

--- sudo/src/main.rs
+++ sudo/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+mod cli_args;
+use clap::{Parser, CommandFactory};
+use cli_args::Cli;
+
+use crate::cli_args::SudoOptions;
+
+
+#[derive(Debug)]
+struct CustomError(String);
+
+fn main() -> Result<(), CustomError> {
+    let args = Cli::parse();
+    let captured = SudoOptions::from(args.clone());
+    println!("captured: {:?}", captured);
+    Ok(())
+}
+
+
+#[test]
+fn verify_cli() {
+    Cli::command().debug_assert()
+}
